e9a3336b928620f58b9d10d24596578bd0267cd3
Dev <dmb4086@rit.edu>
2020-08-25 10:45:00 -0400

    added docs

--- Week 1/python/parser.py
+++ Week 1/python/parser.py
@@ -1,12 +1,31 @@
+"""
+@author Dev Bhatt
+
+ - Assignment 1 using python
+
+ - How to run this on your own machine:
+        Go to the directory where parser.py is present on
+        your computer by issuing the command 'cd' on your
+        terminal ( cd $PATH )
+
+        then simply type 'python parser.py'
+"""
 import json
 
+# assigning variables
+list1 = []
+
+"""
+    For readability I have divided the operations into 2 functions,
+    file_handler just opens the JSON file and passes the objects to parser()
+    which then models the data
+"""
 def file_handler():
     with open("data.json") as json_file:
         data = json.load(json_file)
 
         parser(data)
 
-list1 = []
 
 def parser(data):
     count = 0
